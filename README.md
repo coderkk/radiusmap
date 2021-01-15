8a3388eececd3fe88ba05e7b47fbc2ffe584f9df
coderkk <yhlaikk@gmail.com>
2021-01-15 12:20:44 +0800

    Edit about page

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# maps-range
+# Radius Map
+Since 13 Jan 2021, Malaysia government implement MCO, CMCO and RMCO to fight against COVID-19. Those implemented MCO States have set (10) ten-kilometre travel limit from house. This map will show you 10km lockdown limit. You can move around the pin to locate your house position, so that you can monitor or plan what place you can travel.
 
 ## Project setup
 ```
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,10 @@
   <div class="about mx-5">
     <h1 class="font-black sm:text-3xl text-5xl mt-10">10km lockdown limit</h1>
     <p class="mt-5 text-left text-2xl">
-      Since 13 Jan 2021, Malaysia government implement MCO, CMCO and RMCO to flight with Covid-19. Those implemented MCO States have set (10) ten-kilometre travel limit from house. This map will show you 10km lockdown limit. You can move around the pin to find your house, so that you can monitor or plan what place you can travel.
+      Since 13 Jan 2021, Malaysia government implement MCO, CMCO and RMCO to fight against COVID-19. Those implemented MCO States have set (10) ten-kilometre travel limit from house. This map will show you 10km lockdown limit. You can move around the pin to locate your house position, so that you can monitor or plan what place you can travel.
     </p>
+    <p>
+      Stay safe!
+    </P>
   </div>
 </template>
